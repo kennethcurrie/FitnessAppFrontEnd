ff8045c371d405bffcf10535e4ac08e977eccdfb
kennethcurrie <kjcurrie@comcast.net>
2019-02-21 14:40:29 -0500

    Added conent to admin's all users page

--- fitnessappfrontend/src/components/allUsers/allUsers.component.tsx
+++ fitnessappfrontend/src/components/allUsers/allUsers.component.tsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './allUsers.scss';
+
+const users = [
+  {Fullname: 'John Smith', Username: 'Jsmith', Email: 'jsmith@gmail.com'},
+  {Fullname: 'Joe Smitty', Username: 'Jsmitty', Email: 'jsmity@gmail.com'},
+  {Fullname: 'Alice Data', Username: 'adata', Email: 'adata@hotmail.com'},
+  {Fullname: 'Joe Smitty', Username: 'Jsmitty', Email: 'jsmity@gmail.com'},
+];
 
 export class AllUsersComponent extends React.Component<any, any> {
   render() {
     return (
+      <div id='allUsers'>
         <h1>All Users Component!</h1>
+        <table>
+          <tr>
+            <th>Username</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+          {users.map(function(listValue) {
+            return (
+              <tr>
+                <td>
+                  <Link to={'admin/users/edit/' + listValue.Username}>{listValue.Username}</Link>
+                </td>
+                <td>{listValue.Fullname}</td>
+                <td><a href={'mailto:' + listValue.Email}>{listValue.Email}</a></td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
     );
   }
 }
