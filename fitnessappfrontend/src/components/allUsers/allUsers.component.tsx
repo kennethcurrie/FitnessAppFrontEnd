a547935545cc0a78c31335a1c1f6d112391a326e
kennethcurrie <kjcurrie@comcast.net>
2019-03-01 20:20:18 -0500

    Revert "Revert "all pages now refer to one file""
    This reverts commit 2f2d8067f66442a7b1a21dd1d790bed2c763bdbb.

--- fitnessappfrontend/src/components/allUsers/allUsers.component.tsx
+++ fitnessappfrontend/src/components/allUsers/allUsers.component.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import './allUsers.scss';
 import { IUser } from '../../redux/interfaces';
 import { IState } from '../../redux/interfaces';
 import { connect } from 'react-redux';
